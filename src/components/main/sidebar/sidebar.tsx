a13e650f69d9362a8bae4ad99d6c8c6c4122afc0
SIeiv <maderator000@gmail.com>
2024-12-08 18:16:58 +0300

    v0.2.3d

--- src/components/main/sidebar/sidebar.tsx
+++ src/components/main/sidebar/sidebar.tsx
@@ -3,7 +3,7 @@ import {LogOut, Newspaper, Phone} from "lucide-react";
 
 const Sidebar = () => {
     return (
-        <div className={"w-52 h-[872px] sticky top-[128px] flex flex-col justify-between"}>
+        <div className={"w-52 h-[500px] sticky top-[128px] flex flex-col justify-between"}>
             <div className={"flex flex-col gap-2"}>
                 <SidebarButton variation={"active"} title={"Посты"} icon={<Newspaper/>}/>
                 <SidebarButton title={"Контакты"} icon={<Phone />}/>
