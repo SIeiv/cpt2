64ae7aebeb4fe5c58bddfe29706f8a5df19a5b4b
SIeiv <maderator000@gmail.com>
2024-12-08 17:26:39 +0300

    v0.2.3b

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-    content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+    content: ["./src/**/*.{ts,tsx,js,jsx,html}"],
     theme: {
         extend: {
             colors: {
