30e867d831e8038e8cd699cea1497e9ab0ff31d6
SIeiv <maderator000@gmail.com>
2024-12-14 14:13:32 +0300

    v0.2.4b

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import Main from "@/pages/main.tsx";
 import DebugMenu from "@/components/func/debug-menu.tsx";
 import "@/App.css";
 import Redirect from "@/components/func/redirect.tsx";
-import {useState} from "react";
-import {IPost} from "@/types.ts";
 
 function App() {
     return (
